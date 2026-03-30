d0e27a12f150c7d570884756b566e14bf2c7be41
sashe50 <sachin.shetty2@ingka.com>
2026-03-30 13:00:33 +0200

    topic: functions covered

--- src/mainfolder/methods.go
+++ src/mainfolder/methods.go
@@ -52,4 +52,90 @@ func meth() {
 
 	// 3. Variadic function
 	fmt.Println("Sum:", sumAll(1, 2, 3, 4, 5))
+
+	AnanamousFunction()
+	Higher1()
+	fmt.Println(ReturnFunction())
+	fmt.Println(ReturnFunction()("Go", "Language"))
+
+	pos := adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(pos(i))
+	}
+}
+
+func AnanamousFunction() {
+
+	// 1. Immediately Invoked Function Expression (IIFE)
+	func() {
+		fmt.Println("This runs immediately!")
+	}()
+
+	// 2. Anonymous function assigned to a variable
+	add := func(a, b int) int {
+		return a + b
+	}
+	fmt.Println("Sum using anonymous function:", add(3, 4))
+
+	// 3. Passing an anonymous function as an argument
+	numbers := []int{1, 2, 3, 4, 5}
+	processNumbers(numbers, func(n int) {
+		fmt.Println("Processing number:", n)
+	})
+
+	// 4. Returning an anonymous function (closure)
+	incrementer := makeIncrementer()
+	fmt.Println("Incrementer:", incrementer()) // 1
+	fmt.Println("Incrementer:", incrementer()) // 2
+}
+
+// Helper function for example 3
+func processNumbers(nums []int, f func(int)) {
+	for _, n := range nums {
+		f(n)
+	}
+}
+
+// Helper function for example 4
+func makeIncrementer() func() int {
+	count := 0
+	return func() int {
+		count++
+		return count
+	}
+}
+
+//Higher order functions
+
+func HigherOrderr1(a func(p, q string) string) {
+	fmt.Println(a("Go", "Lang"))
+}
+
+func Higher1() {
+	value := func(p, q string) string {
+		return p + q + "Program"
+	}
+	HigherOrderr1(value)
+}
+
+//Return function
+
+func ReturnFunction() func(i, j string) string {
+	mf := func(i, j string) string {
+		return i + j + "Program"
+	}
+
+	return mf
+}
+
+//closure function
+
+func adder() func(int) int {
+	sum := 0
+
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+
 }
